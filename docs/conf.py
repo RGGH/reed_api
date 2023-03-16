a296c6a802dc7b63a585b67dfe6e14bc6396b1c2
RGGH <pisscompany@gmail.com>
2023-03-16 18:14:24 +0000

    sphinx demo

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'btcdev'
+copyright = '2023, Mr Moo'
+author = 'Mr Moo'
+release = '0.0.1'
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/src.rst
@@ -0,0 +1,21 @@
+src package
+===========
+
+Submodules
+----------
+
+src.sc1 module
+--------------
+
+.. automodule:: src.sc1
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: src
+   :members:
+   :undoc-members:
+   :show-inheritance:
