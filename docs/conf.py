8542864a1e25f2eccf6c7d9dff81645ea9d2dc88
Red and Green <62441426+RGGH@users.noreply.github.com>
2023-03-18 15:50:35 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'btcdev'
+project = 'reed_api'
 copyright = '2023, Mr Moo'
 author = 'Mr Moo'
 release = '0.0.1'
